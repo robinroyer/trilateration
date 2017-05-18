7bb44836bcdb47994f2d9883446fcb120d23207f
robinroyer <ktyroby@hotmail.fr>
2017-05-18 15:54:19 +0200

    [edit] init the lib for pypi distribution

---
+++ setup.py
@@ -0,0 +1,14 @@
+#from distutils.core import setup
+from setuptools import setup
+setup(
+  name = 'trilateration',
+  packages = ['trilateration'],
+  version = '0.1',
+  description = 'Lib to find TDOA / TOA best solution from 3 circle',
+  author = 'Royer Robin',
+  author_email = 'ktyroby@hotmail.fr',
+  url = 'https://github.com/robinroyer/trilateration',
+  download_url = 'https://github.com/robinroyer/trilateration/archive/0.1.tar.gz',
+  keywords = ['trilateration', 'toa', 'tdoa'],
+  classifiers = [],
+)
