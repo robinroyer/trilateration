8196736ff90da80ec1649d0d7f250624cea9e1e8
robinroyer <ktyroby@hotmail.fr>
2017-05-19 17:27:18 +0200

    [Edit] refactoring to seperate utils and trilateration

--- trilateration/test_trilateration.py
+++ trilateration/test_trilateration.py
@@ -1,23 +1,13 @@
 import unittest
 
-from trilateration import trilateration, circle, point, projection
+from trilateration import trilateration
+from utils import circle, point, projection
 # do not forget to use nose2 at root to run test
 
  
 class Test_trilateration(unittest.TestCase):
     
     # =============================================== OBJECT UNIT TEST
-    def test_circle_creation(self):
-        c = circle(point(48.84, 2.26), 300)
-        self.assertEqual(c.center.lat, 48.84)
-        self.assertEqual(c.center.lon, 2.26)
-        self.assertEqual(c.radius, 300)
-
-    def test_point_creation(self):
-        p1 = point(48.84, 2.26)
-        self.assertEqual(p1.lat, 48.84)
-        self.assertEqual(p1.lon, 2.26)
-
     def test_trilateration_creation(self):
         c1 = circle(point(48.84, 2.26), 300)
         c2 = circle(point(48.84, 2.30), 5)
@@ -69,15 +59,6 @@ class Test_trilateration(unittest.TestCase):
         self.assertTrue(trilat.is_approximation)
 
     # =============================================== ERROR CHECKING
-    def test_negative_radius(self):
-        self.assertRaises(ValueError, lambda: circle(point(0,0), -1))
-
-    def test_bad_circle_parameter(self):
-        self.assertRaises(ValueError, lambda: circle(point(0,0), -1).distance_from_circle_center(42))
-
-    def test_bad_point_parameter(self):
-        self.assertRaises(ValueError, lambda: point(0,0).distance_from_point(42))
-
     def test_only_one_circle(self):
         c1 = circle(point(48.84, 2.30), 5000)
         self.assertRaises(ValueError, lambda: trilateration([c1]))
@@ -93,12 +74,6 @@ class Test_trilateration(unittest.TestCase):
         c3 = circle(point(48.80, 2.30), 3500)
         c4 = circle(point(48.80, 2.30), 3500)
         self.assertRaises(ValueError, lambda: trilateration([c1, c2, c3, c4]))
-
-    def test_incorrect_latitude(self):
-        self.assertRaises(ValueError, lambda: point(30000 +2j,0))
-
-    def test_incorrect_longitude(self):
-        self.assertRaises(ValueError, lambda: point(2,1000000))
 
     def test_incorrect_param_type(self):
         c1 = circle(point(48.84, 2.30), 5000)
