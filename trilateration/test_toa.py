ba8b5633adf206f56916b3dc98258db7cfb9eaf1
robinroyer <ktyroby@hotmail.fr>
2017-05-22 15:11:02 +0200

    [edit] fix test

--- trilateration/test_toa.py
+++ trilateration/test_toa.py
@@ -25,7 +25,7 @@ class Test_toa(unittest.TestCase):
         self.assertEqual(solver._uplinks, [u1, u2, u3])
 
     # =============================================== FUNCTIONNAL TEST
-    def test_complete_approximation(self):
+    def test_trilateration_compute_random(self):
         g1 = gateway(48.84, 2.26)
         g2 = gateway(48.84, 2.30)
         g3 = gateway(48.80, 2.30)
@@ -35,66 +35,112 @@ class Test_toa(unittest.TestCase):
         u3 = uplink(g3, datetime.datetime.now(), int(time.time() * 1000000000))
 
         solver = toa([u1, u2, u3])
+        # user of delta because of 2 occurences of times are different
         self.assertAlmostEqual(solver.geolocalized_device.lat, 48.808139705595316, delta=1.0)
         self.assertAlmostEqual(solver.geolocalized_device.lon, 2.308668, delta=1.0)
 
-    # def test_same_circle(self):
-    #     c1 = circle(point(48.84, 2.26), 3000)
-    #     c2 = circle(point(48.84, 2.26), 3000)
-    #     c3 = circle(point(48.80, 2.30), 3500)
+    def test_trilateration_compute_random(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        g3 = gateway(48.80, 2.30)
 
-    #     trilat = trilateration([c1, c2, c3])
-    #     self.assertAlmostEqual(trilat.geolocalized_device.lat, 48.822284944905945)
-    #     self.assertAlmostEqual(trilat.geolocalized_device.lon, 2.27772629824793)
+        u1 = uplink(g1, datetime.datetime.now(), 1495456868630584064)
+        u2 = uplink(g2, datetime.datetime.now(), 1495456868630585856)
+        u3 = uplink(g3, datetime.datetime.now(), 1495456868630585856)
 
-    # def test_area_intersection(self):
-    #     c1 = circle(point(48.84, 2.26), 3000)
-    #     c2 = circle(point(48.84, 2.30), 5000)
-    #     c3 = circle(point(48.80, 2.30), 3500)
+        solver = toa([u1, u2, u3])
+        # user of delta because of 2 occurences of times are different
+        self.assertAlmostEqual(solver.geolocalized_device.lat, 48.819999, delta=.000001)
+        self.assertAlmostEqual(solver.geolocalized_device.lon, 2.286716, delta=.000001)
 
-    #     trilat = trilateration([c1, c2, c3])
+    def test_same_time(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        g3 = gateway(48.80, 2.30)
 
-    #     self.assertFalse(trilat.is_approximation)
-    #     self.assertAlmostEqual(trilat.geolocalized_device.lat, 48.82313276075889)
-    #     self.assertAlmostEqual(trilat.geolocalized_device.lon, 2.273550537885488)
+        t = int(time.time() * 1000000000)
 
-    # def test_two_circles_intersection(self):
-    #     c1 = circle(point(48.84, 2.26), 3000)
-    #     c2 = circle(point(48.84, 2.30), 5000)
-    #     c3 = circle(point(48.80, 2.30), 3)
+        u1 = uplink(g1, datetime.datetime.now(), t)
+        u2 = uplink(g2, datetime.datetime.now(), t)
+        u3 = uplink(g3, datetime.datetime.now(), int(time.time() * 1000000000))
 
-    #     trilat = trilateration([c1, c2, c3])
+        solver = toa([u1, u2, u3])
+        self.assertFalse(solver.is_resolved)
 
-    #     self.assertTrue(trilat.is_approximation)
+    def test_same_gateway(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        g3 = gateway(48.84, 2.30)
 
-    # # =============================================== ERROR CHECKING
-    # def test_only_one_circle(self):
-    #     c1 = circle(point(48.84, 2.30), 5000)
-    #     self.assertRaises(ValueError, lambda: trilateration([c1]))
+        u1 = uplink(g1, datetime.datetime.now(), int(time.time() * 1000000000))
+        u2 = uplink(g2, datetime.datetime.now(), int(time.time() * 1000000000))
+        u3 = uplink(g3, datetime.datetime.now(), int(time.time() * 1000000000))
 
-    # def test_only_two_circles(self):
-    #     c1 = circle(point(48.84, 2.30), 5000)
-    #     c2 = circle(point(48.80, 2.30), 3500)
-    #     self.assertRaises(ValueError, lambda: trilateration([c1, c2]))
+        solver = toa([u1, u2, u3])
+        self.assertFalse(solver.is_resolved)
 
-    # def test_four_circles(self):
-    #     c1 = circle(point(48.84, 2.30), 5000)
-    #     c2 = circle(point(48.80, 2.30), 3500)
-    #     c3 = circle(point(48.80, 2.30), 3500)
-    #     c4 = circle(point(48.80, 2.30), 3500)
-    #     self.assertRaises(ValueError, lambda: trilateration([c1, c2, c3, c4]))
+    def test_same_uplink(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        g3 = gateway(48.84, 2.30)
 
-    # def test_incorrect_param_type(self):
-    #     c1 = circle(point(48.84, 2.30), 5000)
-    #     c2 = circle(point(48.80, 2.30), 3500)
-    #     self.assertRaises(ValueError, lambda: trilateration([c1, c2, .0])) 
+        t = int(time.time() * 1000000000)
 
-    # def test_incorrect_projection(self):
-    #     c1 = circle(point(48.84, 2.30), 5000)
-    #     c2 = circle(point(48.80, 2.30), 3500)
-    #     c3 = circle(point(48.80, 2.30), 3500)
-    #     projection_system = 42
-    #     self.assertRaises(ValueError, lambda: trilateration([c1, c2, c3], projection_system)) 
+        u1 = uplink(g1, datetime.datetime.now(), int(time.time() * 1000000000))
+        u2 = uplink(g2, datetime.datetime.now(), t)
+        u3 = uplink(g3, datetime.datetime.now(), t)
+
+        solver = toa([u1, u2, u3])
+        self.assertFalse(solver.is_resolved)
+
+    # =============================================== ERROR CHECKING
+    def test_only_one_uplink(self):
+        g1 = gateway(48.84, 2.26)
+        u1 = uplink(g1, datetime.datetime.now(), int(time.time() * 1000000000))
+
+        self.assertRaises(ValueError, lambda: toa([u1]))
+
+    def test_only_two_uplinks(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        u1 = uplink(g1, datetime.datetime.now(), int(time.time() * 1000000000))
+        u2 = uplink(g2, datetime.datetime.now(), int(time.time() * 1000000000))
+
+        self.assertRaises(ValueError, lambda: toa([u1, u2]))
+
+    def test_four_circles(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        g3 = gateway(48.80, 2.30)
+        g4 = gateway(48.80, 2.22)
+
+        u1 = uplink(g1, datetime.datetime.now(), int(time.time() * 1000000000))
+        u2 = uplink(g2, datetime.datetime.now(), int(time.time() * 1000000000))
+        u3 = uplink(g3, datetime.datetime.now(), int(time.time() * 1000000000))
+        u4 = uplink(g4, datetime.datetime.now(), int(time.time() * 1000000000))
+
+        self.assertRaises(ValueError, lambda: toa([u1, u2, u3, u4]))
+
+    def test_incorrect_param_type(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        u1 = uplink(g1, datetime.datetime.now(), int(time.time() * 1000000000)) 
+        u2 = uplink(g2, datetime.datetime.now(), int(time.time() * 1000000000))
+
+        self.assertRaises(ValueError, lambda: toa([u1, u2, .0])) 
+
+    def test_incorrect_projection(self):
+        g1 = gateway(48.84, 2.26)
+        g2 = gateway(48.84, 2.30)
+        g3 = gateway(48.80, 2.30)
+
+        u1 = uplink(g1, datetime.datetime.now(), int(time.time() * 1000000000))
+        u2 = uplink(g2, datetime.datetime.now(), int(time.time() * 1000000000))
+        u3 = uplink(g3, datetime.datetime.now(), int(time.time() * 1000000000))
+
+        solver = toa([u1, u2, u3])
+        projection_system = 42
+        self.assertRaises(ValueError, lambda: toa([u1, u2, u3], projection_system)) 
 
 if __name__ == '__main__':
     unittest.main()
