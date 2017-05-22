a9ba6d671a8e91ce96fa5c8e308cdc5a51455a77
robinroyer <ktyroby@hotmail.fr>
2017-05-22 15:44:20 +0200

    [edit] documentation and utils test

--- trilateration/test_utils.py
+++ trilateration/test_utils.py
@@ -1,6 +1,8 @@
 import unittest
 
-from utils import circle, point, projection
+import time
+import datetime
+from utils import circle, point, projection, gateway, uplink
 # do not forget to use nose2 at root to run test
 
  
@@ -18,6 +20,22 @@ class Test_trilateration(unittest.TestCase):
         self.assertEqual(p1.lat, 48.84)
         self.assertEqual(p1.lon, 2.26)
 
+    def test_gateway_creation(self):
+        g = gateway(48.84, 2.26)
+        self.assertEqual(g.lat, 48.84)
+        self.assertEqual(g.lon, 2.26)
+
+    def test_uplink_creation(self):
+        g = gateway(48.84, 2.26)
+        t = int(time.time() * 1000000000)   
+        d = datetime.datetime.now()
+        u = uplink(g, d, t)
+
+        self.assertEqual(u.gateway.lat, 48.84)
+        self.assertEqual(u.gateway.lon, 2.26)
+        self.assertEqual(u.timestamp, t)
+        self.assertEqual(u.arrival_date, d)
+
     # =============================================== ERROR CHECKING
     def test_negative_radius(self):
         self.assertRaises(ValueError, lambda: circle(point(0,0), -1))
@@ -28,11 +46,24 @@ class Test_trilateration(unittest.TestCase):
     def test_bad_point_parameter(self):
         self.assertRaises(ValueError, lambda: point(0,0).distance_from_point(42))
 
-    def test_incorrect_latitude(self):
+    def test_incorrect_latitude_point(self):
         self.assertRaises(ValueError, lambda: point(30000 +2j,0))
 
-    def test_incorrect_longitude(self):
+    def test_incorrect_longitude_point(self):
         self.assertRaises(ValueError, lambda: point(2,1000000))
+
+    def test_incorrect_latitude_gateway(self):
+        self.assertRaises(ValueError, lambda: gateway(30000 +2j,0))
+
+    def test_incorrect_longitude_gateway(self):
+        self.assertRaises(ValueError, lambda: gateway(2,1000000))
+
+    def test_incorrect_timestamp_uplink(self):
+        g = gateway(48.84, 2.26)
+        self.assertRaises(ValueError, lambda: uplink(g, datetime.datetime.now(), "coucou"))
+
+    def test_incorrect_gateway_uplink(self):
+        self.assertRaises(ValueError, lambda: uplink("coucou", datetime.datetime.now(), int(time.time() * 1000000000)))
 
 if __name__ == '__main__':
     unittest.main()
