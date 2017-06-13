53e40140acc0537135df66aec2a94982ae29da95
robinroyer <ktyroby@hotmail.fr>
2017-06-13 17:36:30 +0200

    [edit] implement adaptative kalman filter

--- trilateration/filter/extended_kalman_filter.py
+++ trilateration/filter/extended_kalman_filter.py
@@ -1,91 +1,228 @@
-from filterpy.kalman import ExtendedKalmanFilter
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from numpy.random import randn
 from math import sqrt
 import sympy
+from scipy.linalg import block_diag
+from filterpy.common import Q_discrete_white_noise
 
-
-class RadarSim(object):
-    """ Simulates the radar signal returns from an object
-    flying at a constant altityude and velocity in 1D. 
-    """
-    
-    def __init__(self, dt, pos, vel, alt):
-        self.pos = pos
-        self.vel = vel
-        self.alt = alt
-        self.dt = dt
-        
-    def get_range(self):
-        """ Returns slant range to the object. Call once 
-        for each new measurement at dt time from last call.
-        """
-        
-        # add some process noise to the system
-        self.vel = self.vel  + .1*randn()
-        self.alt = self.alt + .1*randn()
-        self.pos = self.pos + self.vel*self.dt
-    
-        # add measurement noise
-        err = self.pos * 0.05*randn()
-        slant_dist = math.sqrt(self.pos**2 + self.alt**2)
-        
-        return slant_dist + err
-
-
-class ekf_filter:
+class kf_filter:
 	"""create a gh filter"""
-	def __init__(self, dimx, x, dimz, f):
-		self.filter = ExtendedKalmanFilter(dim_x=dimx, dim_z=dimz)
-		self.filter.x = x
-		self.filter.F = f
+	def __init__(self, dimx, dimz, state, covariance, transitionMat, measurementFunc, dt, noiseCovariance, correlation, dimNoise):
+		self.filter = KalmanFilter(dim_x=dimx, dim_z=dimz)
+		self.filter.x = np.array(state) 			# state (position and velocity)
+		self.filter.F = np.array(transitionMat)  	# state transition matrix
+		self.filter.H = np.array(measurementFunc)	# Measurement function
+		# print correlation.shape
+		self.filter.R *= correlation   	            # measurement uncertainty
+		
+		if np.isscalar(covariance):
+		    self.filter.P *= covariance             # covariance matrix 
+		else:
+		    self.filter.P[:] = covariance      		# [:] makes deep copy
+		
+		if np.isscalar(noiseCovariance):
+		    self.filter.Q = Q_discrete_white_noise(dim=dimNoise, dt=dt, var=noiseCovariance)
+		else:
+		    self.filter.Q[:] = noiseCovariance
 
-	def HJacobian_at(x):
-    """ compute Jacobian of H matrix at x """
-    horiz_dist = x[0]
-    altitude   = x[2]
-    denom = sqrt(horiz_dist**2 + altitude**2)
-    return array ([[horiz_dist/denom, 0., altitude/denom]])
 
-	def hx(x):
-	    """ compute measurement for slant range that
-	    would correspond to state x.
-	    """
-	    
-	    return (x[0]**2 + x[2]**2) ** 0.5
+	def new_measure(self, measure):
+		self.filter.predict()
+		self.filter.update(measure)
+		xs = self.filter.x
+		cov = self.filter.P
+		return xs, cov
 
-	def new_measure(self, measure, hjacob, hx):
-		self.filter.update(measure, hjacob, hx)
-		return self.filter.predict()
+
 
 if __name__ == '__main__':
-	pos = np.array([0.,0.])
-	dpos = np.array([1.,1.])
-	dtime = 1.
-	g = .1
-	h = .0
 
-	filter = gh_filter(pos, dpos, dtime, g, h)
+	def dog():
+		dimx = 2
+		dimz = 1
+		dimNoise = 2
+		dt = .1
+		correlation = 5 # how fast you want to update model (big number is slow convergercence)
+		covariance = 500 # => how close to the measurement you want to be
+		noiseCovariance = .2 # => how do you trust your sensor
+		# x = np.array([0., 0.])
+		x = np.array([0., 4000.]) # bad start
+		measurementFunc = np.array([[1., 0]])
+		stateTransition = np.array([[1., dt],
+									[0., 1.]])
+		kf = kf_filter(dimx , dimz, x, covariance, stateTransition, measurementFunc, dt, noiseCovariance, correlation, dimNoise)
 
-	results = np.array([])
-	measures = np.array([])
-	positions = np.array([])
+		results = np.array([])
+		resultscov = np.array([])
+		measures = np.array([])
+		positions = np.array([])
 
-	for x in xrange(0, 80):
-		noise = np.random.rand(1, 2)
-		pos = np.array([x, x])
-		measure = pos + 20 * (noise - 0.5)
-		#  update the filter
-		res = filter.new_measure(measure)
-		# store data to plot
-		positions = np.append(positions, pos)
-		measures = np.append(measures, measure)
-		results = np.append(results, res[0])
+		epoch = 200
+		for x in xrange(0, epoch):
+			noise1 = np.random.rand(1)
+			noise2 = np.random.rand(1)
+			# pos = np.array([x, x])
+			pos = x
+			if x > epoch/2:
+				pos = epoch - x
+			measure = pos + 20 * (noise1 - 2.5) + 25 * (noise2 + 1.5) # result of 2 gaussians not centered
+			#  update the filter
+			resx, rescov  = kf.new_measure(measure)
+			# print rescov
+			# store data to plot
+			positions = np.append(positions, pos)
+			measures = np.append(measures, measure)
+			results = np.append(results, resx[0])
+			resultscov = np.append(resultscov, rescov[0])
 
-	fig, ax = plt.subplots()
-	ax.plot(results)
-	ax.plot(measures, "*")
-	ax.plot(positions, "x")
-	plt.show()
+		plt.figure(1)
+		plt.subplot(221)
+		plt.plot(resultscov, label="covariance")
+		plt.title("filtre de Kalman")
+		
+		plt.subplot(212)
+		plt.plot(results, label="filtre de kalman")
+		plt.plot(measures, "*", label="mesures")
+		plt.plot(positions, "x", label="position reele")
+		plt.legend(bbox_to_anchor=(.5, .7, .5, 1.),
+				   loc=1,
+				   ncol=1,
+				   mode="expand",
+				   borderaxespad=0.)
+		plt.show()
+
+
+	def car():
+		dimx = 4
+		dimz = 2
+		dimNoise = 2
+		dt = .1
+		correlation = 5 # how fast you want to update model (big number is slow convergercence)
+		covariance = 5 # => how close to the measurement you want to be
+		# noiseCovariance = 0.1  # => how do you trust your sensor
+		noiseCovariance = np.diag([ dt**4/16, dt**3/9, dt**2/4, dt**1])
+		x = np.array([0., 0., 0., 0.])
+		measurementFunc = np.array([[1., 0., 0., 0.],
+									[0., 1., 0., 0.]])
+		stateTransition = np.array([[1., 0., dt, 0.],
+									[0., 1., 0., dt],
+									[0., 0., 1., 0.],
+									[0., 0., 0., 1.]])
+		kf = kf_filter(dimx , dimz, x, covariance, stateTransition, measurementFunc, dt, noiseCovariance, correlation, dimNoise)
+
+		epoch = 500
+		results = np.empty([epoch, 4])
+		resultscov = np.empty([epoch, 4, 4])
+		measures = np.empty([epoch, 2])
+		positions = np.empty([epoch, 2])
+
+		for x in xrange(0, epoch):
+			noise1 = np.random.rand(2)
+			noise2 = np.random.rand(2)
+			if x > 3*epoch/4:
+				pos = np.array([7*epoch/4 - 2*x, 2*x - 2*epoch])
+			elif x > epoch/2:
+				pos = np.array([epoch - x, epoch/4 - x])
+			elif x > epoch/4:
+				pos = np.array([2*x - epoch/2, 3*epoch/4 - 2*x])
+			else:
+				pos = np.array([x - epoch/4, x])
+
+			measure = pos + 20 * (noise1 - 0.7) + 25 * (noise2 - 0.3)
+			#  update the filter
+			resx, rescov = kf.new_measure(measure)
+			# store data to plot
+			positions[x] = pos
+			measures[x] = measure
+			results[x] = resx
+			resultscov[x] = rescov
+		plt.figure(1)
+		plt.subplot(221)
+		plt.plot(np.add.reduce(np.add.reduce(resultscov, axis=1), axis=1), label="covariance")
+		plt.title("filtre de Kalman")
+		plt.subplot(212)
+		plt.plot(results[:,0], results[:,1], label="filtre de kalman", markersize=1)
+		plt.plot(measures[:,0], measures[:,1], "*", label="mesures", markersize=1)
+		plt.plot(positions[:,0], positions[:,1], "x", label="position reele", markersize=1)
+		plt.legend(bbox_to_anchor=(.5, .7, .5, 1.),
+				   loc=1,
+				   ncol=1,
+				   mode="expand",
+				   borderaxespad=0.)
+		plt.show()
+
+	def plain():
+		dimx = 6
+		dimz = 2
+		dimNoise = 2
+		dt = .1
+		correlation = 5 # how fast you want to update model (big number is slow convergercence)
+		covariance = 5 # => how close to the measurement you want to be
+		# noiseCovariance = 0.1  # => how do you trust your sensor
+		# noiseCovariance = np.diag([ dt**4/16, dt**3/9, dt**2/4, dt**1])
+		noiseCovariance = Q_discrete_white_noise(dim=2, dt=dt, var=0.01)
+		noiseCovariance = block_diag(q, q, q)
+
+
+		x = np.array([0., 0., 0., 0.])
+		measurementFunc = np.array([[1., 0., 0., 0., 0., 0.],
+									[0., 1., 0., 0., 0., 0.]])
+
+		stateTransition = np.array([[1., 0., dt, 0., 0.5*dt**2,        0.],
+									[0., 1., 0., dt,        0., 0.5*dt**2],
+									[0., 0., 1., 0.,        dt,        0.],
+									[0., 0., 0., 1.,        0.,        dt],
+									[0., 0., 0., 0.,        1.,        0.],
+									[0., 0., 0., 0.,        0.,        1.]])
+
+		kf = kf_filter(dimx , dimz, x, covariance, stateTransition, measurementFunc, dt, noiseCovariance, correlation, dimNoise)
+
+		epoch = 500
+		results = np.empty([epoch, 4])
+		resultscov = np.empty([epoch, 4, 4])
+		measures = np.empty([epoch, 2])
+		positions = np.empty([epoch, 2])
+
+		for x in xrange(0, epoch):
+			noise1 = np.random.rand(2)
+			noise2 = np.random.rand(2)
+			if x > 3*epoch/4:
+				pos = np.array([7*epoch/4 - 2*x, 2*x - 2*epoch])
+			elif x > epoch/2:
+				pos = np.array([epoch - x, epoch/4 - x])
+			elif x > epoch/4:
+				pos = np.array([2*x - epoch/2, 3*epoch/4 - 2*x])
+			else:
+				pos = np.array([x - epoch/4, x])
+
+			measure = pos + 20 * (noise1 - 0.7) + 25 * (noise2 - 0.3)
+			#  update the filter
+			resx, rescov = kf.new_measure(measure)
+			# store data to plot
+			positions[x] = pos
+			measures[x] = measure
+			results[x] = resx
+			resultscov[x] = rescov
+		plt.figure(1)
+		plt.subplot(221)
+		plt.plot(np.add.reduce(np.add.reduce(resultscov, axis=1), axis=1), label="covariance")
+		plt.title("filtre de Kalman")
+		plt.subplot(212)
+		plt.plot(results[:,0], results[:,1], label="filtre de kalman", markersize=1)
+		plt.plot(measures[:,0], measures[:,1], "*", label="mesures", markersize=1)
+		plt.plot(positions[:,0], positions[:,1], "x", label="position reele", markersize=1)
+		plt.legend(bbox_to_anchor=(.5, .7, .5, 1.),
+				   loc=1,
+				   ncol=1,
+				   mode="expand",
+				   borderaxespad=0.)
+		plt.show()
+
+
+		# real main
+	#dog()
+	car()
+	plain()
--- trilateration/filter/kalman_filter.py
+++ trilateration/filter/kalman_filter.py
@@ -5,7 +5,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from math import sqrt
 import sympy
-
+from scipy.linalg import block_diag
+from filterpy.common import Q_discrete_white_noise
 
 class kf_filter:
 	"""create a gh filter"""
@@ -80,7 +81,7 @@ if __name__ == '__main__':
 		plt.figure(1)
 		plt.subplot(221)
 		plt.plot(resultscov, label="covariance")
-		plt.title("filtre de Kalman")
+		plt.title("filtre de Kalman order 1")
 		
 		plt.subplot(212)
 		plt.plot(results, label="filtre de kalman")
@@ -141,7 +142,71 @@ if __name__ == '__main__':
 		plt.figure(1)
 		plt.subplot(221)
 		plt.plot(np.add.reduce(np.add.reduce(resultscov, axis=1), axis=1), label="covariance")
-		plt.title("filtre de Kalman")
+		plt.title("filtre de Kalman order 1")
+		plt.subplot(212)
+		plt.plot(results[:,0], results[:,1], label="filtre de kalman", markersize=1)
+		plt.plot(measures[:,0], measures[:,1], "*", label="mesures", markersize=1)
+		plt.plot(positions[:,0], positions[:,1], "x", label="position reele", markersize=1)
+		plt.legend(bbox_to_anchor=(.5, .7, .5, 1.),
+				   loc=1,
+				   ncol=1,
+				   mode="expand",
+				   borderaxespad=0.)
+		plt.show()
+
+	def plain():
+		dimx = 6
+		dimz = 2
+		dimNoise = 2
+		dt = .1
+		correlation = 5 # how fast you want to update model (big number is slow convergercence)
+		covariance = 500 # => how close to the measurement you want to be
+		# noiseCovariance = 0.1  # => how do you trust your sensor
+		noiseCovariance = np.diag([ dt**6/36, dt**5/25,dt**4/16, dt**3/9, dt**2/4, dt**1])
+
+		x = np.array([0., 0., 0., 0., 0., 0.])
+		measurementFunc = np.array([[1., 0., 0., 0., 0., 0.],
+									[0., 1., 0., 0., 0., 0.]])
+
+		stateTransition = np.array([[1., 0., dt, 0., 0.5*dt**2,        0.],
+									[0., 1., 0., dt,        0., 0.5*dt**2],
+									[0., 0., 1., 0.,        dt,        0.],
+									[0., 0., 0., 1.,        0.,        dt],
+									[0., 0., 0., 0.,        1.,        0.],
+									[0., 0., 0., 0.,        0.,        1.]])
+
+		kf = kf_filter(dimx , dimz, x, covariance, stateTransition, measurementFunc, dt, noiseCovariance, correlation, dimNoise)
+
+		epoch = 500
+		results = np.empty([epoch, 6])
+		resultscov = np.empty([epoch, 6, 6])
+		measures = np.empty([epoch, 2])
+		positions = np.empty([epoch, 2])
+
+		for x in xrange(0, epoch):
+			noise1 = np.random.rand(2)
+			noise2 = np.random.rand(2)
+			if x > 3*epoch/4:
+				pos = np.array([7*epoch/4 - 2*x, 2*x - 2*epoch])
+			elif x > epoch/2:
+				pos = np.array([epoch - x, epoch/4 - x])
+			elif x > epoch/4:
+				pos = np.array([2*x - epoch/2, 3*epoch/4 - 2*x])
+			else:
+				pos = np.array([x - epoch/4, x])
+
+			measure = pos + 20 * (noise1 - 0.7) + 25 * (noise2 - 0.3)
+			#  update the filter
+			resx, rescov = kf.new_measure(measure)
+			# store data to plot
+			positions[x] = pos
+			measures[x] = measure
+			results[x] = resx
+			resultscov[x] = rescov
+		plt.figure(1)
+		plt.subplot(221)
+		plt.plot(np.add.reduce(np.add.reduce(resultscov, axis=1), axis=1), label="covariance")
+		plt.title("filtre de Kalman order 2")
 		plt.subplot(212)
 		plt.plot(results[:,0], results[:,1], label="filtre de kalman", markersize=1)
 		plt.plot(measures[:,0], measures[:,1], "*", label="mesures", markersize=1)
@@ -155,5 +220,6 @@ if __name__ == '__main__':
 
 
 		# real main
-	#dog()
+	# dog()
 	car()
+	# plain()
